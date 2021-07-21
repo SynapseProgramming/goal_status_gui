eac380521bf81d0683ed0b649893c5576f1ba6d4
Roald Ong <roald.ong@gmail.com>
2021-07-21 13:52:13 +0800

    enabled audio to be re-loaded when switching states

--- preload.js
+++ preload.js
@@ -1,14 +1,21 @@
-  require('electron').ipcRenderer.on('received_state', (event, message) => {
-    var audio = new Audio();
+var audio = new Audio();
 
-    if (message == "0") {
-      document.getElementById("circle").style.background = "green";
-    } else if (message == "1") {
-      document.getElementById("circle").style.background = "yellow";
-      audio.src = "Ricardo.mp3"
-      audio.play();
-    } else if (message == "2") {
-      document.getElementById("circle").style.background = "red";
-    }
 
-  })
+require('electron').ipcRenderer.on('received_state', (event, message) => {
+
+  if (message == "0") {
+
+
+    document.getElementById("circle").style.background = "green";
+  } else if (message == "1") {
+    document.getElementById("circle").style.background = "yellow";
+    audio.pause();
+    audio.currentTime = 0;
+    audio.src = "Ricardo.mp3"
+    audio.load()
+    audio.play();
+  } else if (message == "2") {
+    document.getElementById("circle").style.background = "red";
+  }
+
+})
