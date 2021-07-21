034283aa805a3bd4aea3ca42bbf089379b820c82
Roald Ong <roald.ong@gmail.com>
2021-07-21 11:40:29 +0800

    added in different colours for the circle for different statuses

--- preload.js
+++ preload.js
@@ -1,4 +1,10 @@
   require('electron').ipcRenderer.on('received_state', (event, message) => {
-    document.getElementById("goal_state").innerText = message
+    if (message == "0") {
+      document.getElementById("circle").style.background = "green";
+    } else if (message == "1") {
+      document.getElementById("circle").style.background = "yellow";
+    } else if (message == "2") {
+      document.getElementById("circle").style.background = "red";
+    }
 
   })
