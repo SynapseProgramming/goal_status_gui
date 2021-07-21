d124538f7b8c83eccb053f326e9407a50f9cf2d8
Roald Ong <roald.ong@gmail.com>
2021-07-21 14:21:01 +0800

    added songs for each status

--- preload.js
+++ preload.js
@@ -4,17 +4,23 @@ var audio = new Audio();
 require('electron').ipcRenderer.on('received_state', (event, message) => {
 
   if (message == "0") {
-
-
+    audio.pause();
+    audio.src = "Rick.mp3"
+    audio.play();
     document.getElementById("circle").style.background = "green";
+
   } else if (message == "1") {
     document.getElementById("circle").style.background = "yellow";
     audio.pause();
-    audio.currentTime = 0;
-    audio.src = "Ricardo.mp3"
-    audio.load()
+    audio.src = "Tokyo.mp3"
     audio.play();
+
   } else if (message == "2") {
+
+    audio.pause();
+    audio.src = "Ricardo.mp3"
+    audio.play();
+
     document.getElementById("circle").style.background = "red";
   }
 
